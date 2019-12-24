573d37f0e61ffd6a009663a83648f45a62b8d5a0
Billy Luo <32431813+Billy1900@users.noreply.github.com>
2019-12-24 10:39:34 +0800

    Add files via upload

---
+++ exercise3/test3.c
@@ -0,0 +1,26 @@
+int fact(int n){
+	int temp;
+    if(n==1)
+        return n;
+    else{
+    	temp=(n*fact(n-1));
+    	return temp;
+    }
+}
+
+
+int main()
+{
+    int result,times;
+    times=read();
+    for(int i=0;i<times;i++){
+    	int m = read();
+    	if( m > 1) {
+        	result=fact(m);
+        }
+        else
+            result = 1;
+        print(result);
+    }
+    return 0;
+}
